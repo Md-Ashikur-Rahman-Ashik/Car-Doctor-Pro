0f1ddf4b65437a4abde53c4e63920e7e7d167937
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-24 09:58:38 +0600

    Initial project setup completed

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="container rounded-xl mx-auto">
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h2 className="text-6xl text-center font-bold">Car Doctor Pro</h2>
+      </main>
+    </div>
+  );
+}
