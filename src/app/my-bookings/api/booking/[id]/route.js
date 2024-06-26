2b8c492a6b865fe4b3bb5d0049ed98303b20298c
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-26 06:08:37 +0600

    Patched API for update

--- src/app/my-bookings/api/booking/[id]/route.js
+++ src/app/my-bookings/api/booking/[id]/route.js
@@ -14,19 +14,32 @@ export const DELETE = async (request, { params }) => {
   }
 };
 
-// export const PATCH = async (request, { params }) => {
-//   const db = await connectDB();
-//   const bookingsCollection = db.collection("bookings");
-//   try {
-//     const res = await bookingsCollection.deleteOne({
-//       _id: params.id,
-//     });
-//     // console.log(res);
-//     return Response.json({ message: "Deleted the booking", response: res });
-//   } catch (error) {
-//     return Response.json({ message: "Something went wrong" });
-//   }
-// };
+export const PATCH = async (request, { params }) => {
+  const db = await connectDB();
+  const bookingsCollection = db.collection("bookings");
+  const { date, phone, address } = await request.json();
+  try {
+    const res = await bookingsCollection.updateOne(
+      {
+        _id: params.id,
+      },
+      {
+        $set: {
+          date,
+          phone,
+          address,
+        },
+      },
+      {
+        upsert: true,
+      }
+    );
+    // console.log(res);
+    return Response.json({ message: "Updated the booking", response: res });
+  } catch (error) {
+    return Response.json({ message: "Something went wrong" });
+  }
+};
 
 export const GET = async (request, { params }) => {
   const db = await connectDB();
