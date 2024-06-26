339e9c02e72a2430e1b5a2e0b5dfcad1c458a286
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-26 09:09:11 +0600

    Removed about section

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Banner from "./Banner";
-import About from "./About";
 import Services from "./Services";
 
 const HomePage = () => {
   return (
     <div className="container rounded-xl mx-auto">
       <Banner></Banner>
-      <About></About>
       <Services></Services>
     </div>
   );
